bccd9b4d1c0f513966769ed658653fa62ba3de8d
5un9hun <ben4109@naver.com>
2025-07-25 14:03:56 +0900

    add server

---
+++ test.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import os, subprocess
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/encode', methods=['POST'])
+def encode():
+    # append encode code
+
+    string = request.form['dec_input']
+    cmd = f'echo {string} | base64'
+    output = subprocess.check_output(cmd, shell=True).decode().strip()
+    return render_template('index.html', encoded=output)
+
+
+@app.route('/decode', methods=['POST'])
+def decode():
+    # append decode code
+
+    string = request.form['enc_input']
+    cmd = f'echo {string} | base64 -d'
+    output = subprocess.check_output(cmd, shell=True).decode().strip()
+    return render_template('index.html', decoded=output)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
+
